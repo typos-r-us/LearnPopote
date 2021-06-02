091c6229d307a2342abdd0940c210b546bdfce9b
Kenneth Mwenda <kennethmwenda@gmail.com>
2021-06-02 22:03:52 +0300

    added file tree

--- app/src/main/java/com/kennethmwenda/learnpopote/OnBoardingFragment3.kt
+++ app/src/main/java/com/kennethmwenda/learnpopote/OnBoardingFragment3.kt
@@ -11,28 +11,8 @@ import kotlinx.android.synthetic.main.fragment_on_boarding3.*
 import java.util.*
 import kotlin.concurrent.schedule
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OnBoardingFragment3.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OnBoardingFragment3 : Fragment(), View.OnClickListener {
-//    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -46,13 +26,13 @@ class OnBoardingFragment3 : Fragment(), View.OnClickListener {
         // return the inflated layout for this fragment
         return view1
     }
-////////
+
     companion object {
         fun newInstance(): OnBoardingFragment3 {
             return OnBoardingFragment3()
         }
     }
-////////
+
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.fab_toLogin -> {
@@ -64,31 +44,4 @@ class OnBoardingFragment3 : Fragment(), View.OnClickListener {
             }
         }
     }
-///////
-
-
-
-//    private fun goToLogin(){
-//
-//    }
-
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment OnBoardingFragment3.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//                OnBoardingFragment3().apply {
-//                    arguments = Bundle().apply {
-//                        putString(ARG_PARAM1, param1)
-//                        putString(ARG_PARAM2, param2)
-//                    }
-//                }
-//    }
 }
