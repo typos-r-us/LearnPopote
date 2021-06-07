7262100014dac25350097531708f5281723bb035
Kenneth Mwenda <kennethmwenda@gmail.com>
2021-06-07 04:44:42 +0300

    completed html course screen layout and tests.

--- app/src/main/java/com/kennethmwenda/learnpopote/activities/HtmlCourseActivity.kt
+++ app/src/main/java/com/kennethmwenda/learnpopote/activities/HtmlCourseActivity.kt
@@ -7,10 +7,9 @@ import android.view.View
 //import android.widget.ArrayAdapter
 //import android.widget.Spinner
 import android.widget.*
-import com.google.android.youtube.player.YouTubePlayerSupportFragment
-import com.google.android.youtube.player.YouTubePlayerView
-import com.google.android.youtube.player.YoutubePlayerLP
+import com.google.android.youtube.player.*
 import com.kennethmwenda.learnpopote.R
+import kotlinx.android.synthetic.main.activity_html_course.*
 
 class HtmlCourseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +18,13 @@ class HtmlCourseActivity : AppCompatActivity() {
         val spinner: Spinner = findViewById(R.id.sp_courseTopicName)
         //Create An Array containing topic names
         val topics:MutableList<String> = ArrayList()
-        topics.add("Introduction")
-        topics.add("Review of HTML Elements")
-        topics.add("Inserting Spaces and Line Breaks")
-        topics.add("What is an HTML Table?")
-        topics.add("Creating a Hyperlink")
-        topics.add("Graphic File Formats")
+        topics.add("")
+        topics.add("Introduction to HTML") //https://www.youtube.com/watch?v=iHyAxOb0Au4
+        topics.add("Review of HTML Elements") //https://www.youtube.com/watch?v=Ytcp0zyCUS8
+        topics.add("Inserting Spaces and Line Breaks") //https://www.youtube.com/watch?v=s-hdQE61lQg
+        topics.add("What is an HTML Table?") //https://www.youtube.com/watch?v=u4vDA0Uh0Lw
+        topics.add("Creating a Hyperlink") //https://www.youtube.com/watch?v=q_9u0ipiAro
+        topics.add("Graphic File Formats") //https://www.youtube.com/watch?v=S3UKigcH54o
         //Set Array in Adapter
         val adapter =
             ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, topics)
@@ -34,24 +34,65 @@ class HtmlCourseActivity : AppCompatActivity() {
             //To change body of created functions use File | Settings | File Templates.
             }
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val itemSelected:String = topics[position]
-                val print:String = "$itemSelected selected."
-                Toast.makeText(this@HtmlCourseActivity,print,Toast.LENGTH_SHORT).show()
+                // val itemSelected:String = topics[position]
+                // val print:String = "$itemSelected selected."
+                // Toast.makeText(this@HtmlCourseActivity,print,Toast.LENGTH_SHORT).show()
 
-                //custom youtube class and initialization
-                val ytPlayerSupportFragment = YoutubePlayerLP.newInstance()
-                supportFragmentManager.beginTransaction()
-                    .add(R.id.yt_player1, ytPlayerSupportFragment).commit()
-
-                lateinit var videoID: String
-                val ytPlayer:YouTubePlayerView = findViewById(R.id.yt_player)
-                when(position){
+                // Set topic in page header
+                tv_topicHeader.text=topics[position]
+                // Change the video and notes:
+                when (position){
                     0->{
-                        videoID="iHyAxOb0Au4"
-                        ytPlayer. = videoID
+                        // do nothing
+                    }
+                    1->{
+                        ytCreator("iHyAxOb0Au4")
+                        tv_topicNotes.text= getString(R.string.introToHtml)
+                    }
+                    2->{
+                        ytCreator("Ytcp0zyCUS8")
+                    }
+                    3->{
+                        ytCreator("s-hdQE61lQg")
+                    }
+                    4->{
+                        ytCreator("u4vDA0Uh0Lw")
+                    }
+                    5->{
+                        ytCreator("q_9u0ipiAro")
+                    }
+                    6->{
+                        ytCreator("S3UKigcH54o")
                     }
                 }
+                //
             }
         }
+    }
+    fun ytCreator(videoId:String){
+        // custom youtube class and initialization
+        val ytPlayerSupportFragment = YoutubePlayerLP.newInstance()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.yt_player, ytPlayerSupportFragment).commit()
+
+        ytPlayerSupportFragment.initialize(
+            resources.getString(R.string.apiKey), //IF YOU HAVE NO API KEY IT WONT WORK. But that's actually explained in the docs. So you can google it easily if you don't have one
+            object : YouTubePlayer.OnInitializedListener {
+                override fun onInitializationSuccess(
+                    p0: YouTubePlayer.Provider?,
+                    p1: YouTubePlayer?,
+                    p2: Boolean
+                ) {
+                    p1?.loadVideo("$videoId") // string has to be https://www.youtube.com/watch?v=----------->9ET6R_MR1Ag<---------
+                }
+
+                override fun onInitializationFailure(
+                    p0: YouTubePlayer.Provider?,
+                    p1: YouTubeInitializationResult?
+                ) {
+                    Toast.makeText(this@HtmlCourseActivity,"ERROR INITIATING YOUTUBE",Toast.LENGTH_LONG).show()
+                }
+            })
+        // End custom youtube class and initialization
     }
 }
